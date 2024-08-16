71072de9bab96ff34c5529c91d0d54d9a389937f
usesarch <declan.riedel@protonmail.com>
2024-08-16 10:34:09 +0200

    ui redesign, kinda

--- obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70cb197fc662aec56eeb533f449a6e564ecf544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db10d01ffaa80c324c7d793e524b002ef06338a")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
